0285f4a6c36c7db2e85f2b296e2f7102b97fc5e0
Roberto Borges <robertoigor1977@gmail.com>
2026-04-18 10:49:04 -0300

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'roberto2002',
+  password: 'db.password',
   database: 'arquivex'
 });
 
